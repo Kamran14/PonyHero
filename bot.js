001386509a61d95e2f44683ec698f12154d906a2
Kamran14 <Kamran.tayyab99@gmail.com>
2019-12-01 18:50:32 -0500

    PonyHero

---
+++ bot.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const auth = require('./auth.json');
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(auth.token);
+client.on('message', msg => {
+   // console.log(msg.author.username);
+    //console.log(msg.toString().includes("pony"));
+    /*if(msg.author === "Matt" || msg.author === "Kam" || msg.author === "Ryan"){
+        msg.react(":matt:");
+
+    }*/
+    if(contains(msg,"pony") || contains(msg, "matt") || contains(msg, "Matt") ||  contains(msg, "Pony") || contains(msg, "ponies") || contains(msg, "horse")){
+        msg.react(msg.guild.emojis.find(emoji => emoji.name === 'matt'));
+        //msg.channel.send(':matt:')
+    }
+  });
+
+
+function contains(originalMsg, keyWord){
+    if(originalMsg.author.username != "PonyHero"){
+        console.log(originalMsg.author.toString());
+        return originalMsg.toString().includes(keyWord)
+    }else{
+        return false;
+    }
+}
+client.on('error', err => {
+    console.warn(err);
+   });
